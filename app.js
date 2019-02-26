3a47b06dfad17733a3cf4862095f943eb6f9d4e0
takumi <mangu0714@gmail.com>
2019-02-26 09:41:47 +0000

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 'use strict';
+const memo = new Map();     // 連想配列のmap (キー:順番、値:答え)
+// 0番目と1番目の答えは予め定義されているので、それを格納
+memo.set(0, 0);
+memo.set(1, 1);
+
+// 連想配列が n をキーとした答えを持っていればその値をそのまま返し、
+// そうでない場合は再起関数を計算して求める
 function fib(n){
-    if(n === 0){
-        return 0;
-    } else if(n === 1){
-        return 1;
+    if(memo.has(n)){
+        return memo.get(n);
     }
-    return fib(n - 1) + fib(n - 2);
+    const value = fib(n - 1) + fib(n - 2);
+    memo.set(n, value);
+    return value;
 }
 
 const length = 40;
